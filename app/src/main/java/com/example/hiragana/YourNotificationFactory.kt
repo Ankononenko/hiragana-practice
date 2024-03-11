691929e968fd4be4448f85face14777a84a72bec
Ankononenko <a63.114@yandex.ru>
2024-03-11 10:27:08 +0300

    Save the previous NotificationFactory

--- app/src/main/java/com/example/hiragana/YourNotificationFactory.kt
+++ app/src/main/java/com/example/hiragana/YourNotificationFactory.kt
@@ -1,6 +1,7 @@
 package com.example.hiragana
 
 import android.app.Notification
+import android.graphics.Color
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.pushwoosh.notification.PushMessage
@@ -12,12 +13,14 @@ class YourNotificationFactory : PushwooshNotificationFactory() {
         // customNotification condition can be changed to your logic
         // Currently customNotification is always true
         val customNotification = true
-        if (customNotification && getLargeIcon(pushMessage) != null) {
-            val largeIcon = getLargeIcon(pushMessage)
-            // Make a RemoteView by using the largeIcon from PW control panel
+        if (customNotification) {
+            // Make a RemoteView
             val remoteViews = RemoteViews(getApplicationContext()?.packageName ?: "com.example.hiragana", R.layout.custom_notification_layout)
-            remoteViews.setImageViewBitmap(R.id.your_image_view_id, largeIcon)
             Log.d("YourNotificationFactory", "Using custom notification view")
+            // Set the title text
+            remoteViews.setTextViewText(R.id.notification_title, pushMessage.header)
+            // Set the title color
+            remoteViews.setTextColor(R.id.notification_title, Color.RED) // Change RED to your desired color
             val channelId = addChannel(pushMessage)
             val builder = getApplicationContext()?.let { context ->
                 NotificationCompat.Builder(context, channelId)
@@ -25,14 +28,6 @@ class YourNotificationFactory : PushwooshNotificationFactory() {
                     .setContentText(pushMessage.message)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContent(remoteViews)
-            }
-            // bigPicture is displayed when notification is tapped
-            if (pushMessage.bigPictureUrl != null) {
-                Log.d("bigPictureUrl", "bigPucture set")
-                val bigPicture = getBigPicture(pushMessage)
-                if (builder != null) {
-                    builder.setStyle(NotificationCompat.BigPictureStyle().bigPicture(bigPicture))
-                }
             }
             // Return the created notification
             return builder?.build()
